5aa2a6dd63bb2cafed93a1cd49e1eeb40a3af4db
Pallottoa <35118752+Pallottoa@users.noreply.github.com>
2019-01-21 11:16:25 -0500

    Add files via upload

---
+++ ex001.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 28 16:09:19 2018
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+@author: pallo
+"""
+multiples = []                                #Creates an array for values to be added                        
+n=0
+while n<999:
+    n=n+1
+    if n%3==0 or n%5==0:
+        multiples.append(n)                 #adds values that are divisible by 3 or 5
+        
+print(sum(multiples))  
